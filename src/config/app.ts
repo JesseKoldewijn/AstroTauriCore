5a415dedafc4bb26e9ec91ecc7673ab80799fc5e
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2025-01-30 19:54:18 +0100

    init: core setup

---
+++ src/config/app.ts
@@ -0,0 +1,12 @@
+export const APP_CONFIG = {
+  branding: {
+    title: {
+      absolute: "Astro Tauri",
+      relative: "%s | Astro Tauri",
+    },
+    desc: {
+      absolute: "Astro Tauri - A Astro.js based Tauri application",
+      relative: "%s | Astro Tauri - A Astro.js based Tauri application",
+    },
+  },
+};
